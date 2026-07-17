f81271f3cda6d44b026c97b76195c40af5e57037
nshwankmal405-commits <nshwankmal405@gmail.com>
2026-07-17 11:15:51 +0300

    clean

--- app/api/categories/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { db } from "@/db";
-import { categories } from "@/db/schema";
-
-export async function GET() {
-  const result = await db.select().from(categories);
-  return NextResponse.json({ categories: result });
-}
